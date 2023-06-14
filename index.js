a369fffc9584c893aa2473441a75fc31d79b8bc8
collinsmathinji <125723113+collinsmathinji@users.noreply.github.com>
2023-06-14 08:44:21 +0300

    messaging

---
+++ index.js
@@ -0,0 +1,21 @@
+    const search = function () {
+    const searchbox = document.getElementById("input").value.toUpperCase();
+    const storeitems = document.getElementById("product");
+    const product = document.querySelectorAll("#all");
+    const pname = storeitems.getElementsByTagName("h2");
+  
+    for (var i = 0; i < pname.length; i++) {
+      let match = product[i].getElementsByTagName("h2")[0];
+  
+      if (match) {
+        let textvalue = match.textContent || match.innerHTML;
+  
+        if (textvalue.toUpperCase().indexOf(searchbox) > -1) {
+          product[i].style.display = "";
+        } else {
+          product[i].style.display = "none";
+        }
+      }
+    }
+  };
+  
